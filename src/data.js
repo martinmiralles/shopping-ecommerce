09b71a8ac78c19882d3187782c29cb25bd3cc59d
Martin Miralles <martinmiralles66@gmail.com>
2021-12-27 23:13:40 -0500

    Initial commit, and create navbar and pure react slider

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://media.gettyimages.com/photos/photo-of-young-curly-girl-picture-id1182617834?s=2048x2048",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "#f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://media.gettyimages.com/photos/beautiful-woman-with-voluminous-curly-hairstyle-picture-id1189891152?s=2048x2048",
+    title: "AUTUMN SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "#fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://media.gettyimages.com/photos/her-beauty-makes-it-hard-not-to-stare-picture-id1297633362?s=2048x2048",
+    title: "WINTER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "#fbf0f4",
+  },
+];
