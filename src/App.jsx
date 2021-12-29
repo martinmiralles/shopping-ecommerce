f6fed27e05739ae7082489c554f69bab407b9ee8
Martin Miralles <martinmiralles66@gmail.com>
2021-12-29 15:54:43 -0500

    Created register and login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <Product></Product>;
+  return <Login></Login>;
 };
 
 export default App;
